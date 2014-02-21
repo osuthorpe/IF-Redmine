87fc4606a78787050ad316fe30333b6443f513d3
Alex Thorpe <osuthorpe@gmail.com>
2014-02-21 12:50:08 -0800

    precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module RedmineApp
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+    
+    config.assets.initialize_on_precompile = false
 
     config.active_record.store_full_sti_class = true
     config.active_record.default_timezone = :local
